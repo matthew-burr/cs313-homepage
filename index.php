78b10409216a8fd317b961f41125bb733fb146f2
Matthew Burr <matt.d.burr@gmail.com>
2018-01-18 19:36:55 -0600

    Updated home page content

--- index.php
+++ index.php
@@ -3,15 +3,42 @@
   <head>
     <title>Matt Burr's Home</title>
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
   <body>
-    <nav class="navbar navbar-default">
-      <a class="navbar-brand" href="#">MB</a>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">MB</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">home</a></li>
+          <li><a href="assignments.php">assignments</a></li>
+        </ul>
+      </div>
     </nav>
-    <div class="jumbotron">
-      <h1>Matt Burr's Home</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Matt Burr's Homepage</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <img src="MattBurr.jpg" style="width:360px; border:1px solid black;" />
+        </div>
+        <div class="col-md-8">
+          <h2>About Me</h2>
+          <p>I love to program and have been programming since I was a kid. I started with BASIC on our Atari, and later turned to Pascal on our PC. Over
+          the years, I've programmed in BASIC, Pascal, Visual Basic, C, C++, C#, Java, Perl, and several other languages.</p>
+          <p>In my career, I've focused on databases and business intelligence, and I've worked with these technologies for nearly 20 years. I've worked
+          at large and small companies in diverse industries. Although I've been successful, I find that I really enjoy application programming and I'm 
+          building my skillset so that I can move into application development.</p>
+          <p>Outside of work, my hobbies are diverse. I love to read, particularly fiction. I'm also an amateur magician and member of the Academy of 
+          Magical Arts. I enjoy board and card games, as well.</p>
+        </div>
+      </div>
     </div>
   </body>
 </html>
