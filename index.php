ca1208267f9da33126885837ac263f558c14234c
Matthew Burr <matt.d.burr@gmail.com>
2018-01-19 22:22:09 -0600

    Made carousel fade images

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Matt Burr's Home</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.js" integrity="sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="main.css" />
   </head>
--- main.js
+++ main.js
@@ -8,11 +8,14 @@ $(document).ready(function() {
 function startCarousel() {
   var nextImage = 0;
   setInterval(function() {
-    showNextImage(nextImage);
-    nextImage = (nextImage + 1) % imageFiles.length;
+    $("#myPicture").fadeOut("slow", function() {
+      showNextImage(nextImage);
+      nextImage = (nextImage + 1) % imageFiles.length;
+    });
   }, 3000);
 }
 
 function showNextImage(nextImage) {
-  $("#myPicture").attr("src") = imageFiles[nextImage];
+  $("#myPicture").attr("src", imageFiles[nextImage]);
+  $("#myPicture").fadeIn("slow");
 }
