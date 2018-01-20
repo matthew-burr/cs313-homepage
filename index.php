4d08e814421ceb4a71be10bdb2e72ab52e2e7534
Matthew Burr <matt.d.burr@gmail.com>
2018-01-19 20:39:14 -0600

    Updated content

--- index.php
+++ index.php
@@ -36,14 +36,66 @@
           </div>
         </div>
         <div class="col-md-8">
-          <h2>About Me</h2>
-          <p>I love to program and have been programming since I was a kid. I started with BASIC on our Atari, and later turned to Pascal on our PC. Over
-          the years, I've programmed in BASIC, Pascal, Visual Basic, C, C++, C#, Java, Perl, and several other languages.</p>
-          <p>In my career, I've focused on databases and business intelligence, and I've worked with these technologies for nearly 20 years. I've worked
-          at large and small companies in diverse industries. Although I've been successful, I find that I really enjoy application programming and I'm 
-          building my skillset so that I can move into application development.</p>
-          <p>Outside of work, my hobbies are diverse. I love to read, particularly fiction. I'm also an amateur magician and member of the Academy of 
-          Magical Arts. I enjoy board and card games, as well.</p>
+          <h2>Magic</h2>
+          <p>When I was a little boy, maybe 4 years old, I stumbled across a magic kit belonging to my father. Inside were all sorts of tricks with foam balls,
+          paddles, cards, and the like. My favorite involved a little sheet of rubber that you could stretch across the mouth of a cup, and then place a coin atop
+          it. With a simple tap, the coin would pass through the rubber sheet and into the cup. I spent hours playing with that. That magic kit was my first 
+          introduction to the art of magic and opened the door to a life-long interest and study of the art.</p>
+          <h3>Magic as a Kid</h3>
+          <p>After the discovery of that magic kit, I started to collect tricks wherever I could find them. For a time, McDonald's gave out little tricks with
+          their Happy Meals, and I collected all of them. I revelled in showing off to a store cashier how I could push nails through a coin (one of the many
+          Happy Meal tricks).</p>
+          <p>Another favorite source was the magic shop on Main Street in Disneyland. No trip to the park was complete without a visit to the magic shop, and
+          I always left with at least one new magic trick. When I'd visit my grandparents in Sacramento, we almost always took a trip to Old Town Sacramento,
+          and there was a magic shop there that I always visited, too. In this way, I accumulated several essentially self-working tricks that I loved to
+          play with and perform.</p>
+          <p>My father, though, also taught me some basic tricks that actually required a bit of skill. Once or twice, I performed at a Cub Scout pack meeting;
+          working with my dad, I put together a routine involving several classic tricks, but most notable of them all was the classic cups and balls - one of
+          the oldest known tricks in magic history.</p>
+          <p>About the last performance I did as a kid was for my sixth-grade class. By this time, I'd accumulated several more advanced tricks and put together
+          a routine of 4 or 5 of them to perform for my class. My favorite involved a color changing paddle, and I loved fooling the girls in class with it.</p>
+          <h3>Magic as a Teen</h3>
+          <p>As I entered my teenage years, I turned to books to learn more about magic. I stepped away from prepackaged tricks that seldom required much skill
+          and started learning sleight-of-hand, which I found much more impressive. I began working on coin and card tricks, but I was still young and many of
+          the techniques I wanted to learn simply required larger hands and more dexterity than I had. I spent hours upon hours in front of my bathroom mirror
+          trying to learn how to performa complex card pass, fully aware of how visible was everything I did. I became self-conscious and stopped performing
+          even though I continued to practice and learn.</p>
+          <p>My father was a member of a couple of magic clubs. Locally, he was a member of the International Brotherhood of Magicians, which had a chapter
+          in our city. Sometimes, I'd go with him to club meetings. He was also a member of the Academy of Magical Arts; their clubhouse, the Magic Castle,
+          in Hollywood, is one of the premier private magic clubs in all the world. Normally, you have to be 21 or older to visit the club, but a few times
+          a year, they had special events that children could attend, and my dad would take me. I loved that place, set in a Victorian-style mansion in the
+          Hollywood Hills, the building was filled with dark corners and magical haunts. I always looked forward to the chance to visit and yearned for the
+          day I could be old enough to attend on my own.</p>
+          <h3>Magic as an Adult</h3>
+          <p>By the time I reached adulthood, I still hadn't performed in years and was still self-conscious about performing. I'd accumulated a fairly 
+          sizeable library of magic books and a lot of knowledge of skill and theory. I did occasionally relish making coins and other small objects vanish
+          for young kids; the look of surprise on their face was always a joy.</p>
+          <p>After coming home from my mission, I set out to visit the Magic Castle. I discovered they taught classes and that while taking classes, I would
+          have a temporary membership with access to the club. In the classes, I learned some new tricks, and I also developed some confidence, but when the
+          classes ended, so did my club access.</p>
+          <p>A few years later, I had a chance to take classes again. This time, I was determined to go as far as I could. After the first level of classes,
+          I enrolled for the next level. The second level focused on preparing students to become members of the club by putting together a small routine
+          to perform in an audition.</p>
+          <p>Membership in the Academy of Magical Arts comes in two varieties. Associate Members essentially get in by paying hefty dues. Magician Members,
+          have lower dues but must audition to get in. Auditions comprise a small performance before a membership committee.</p>
+          <p>I put together a routine of three card tricks. By now, cards had become my primary medium for magic. Over the course of several weeks, I performed
+          these tricks regularly before friends and family. I honed my technique and presentation, and I increased my confidence. The more often I performed
+          and succeeded at wowing others, the more confident I became.</p>
+          <p>Finally, the night of the audition came. I was the second person to audition that evening. They called my name and led me down some stairs to
+          one of the smaller performance venues at the Castle. There was a small card table surrounded by 5 or 6 members of the Academy. We discussed
+          briefly my interest in and history with magic, and then I performed. Something magical happened that evening. All of my fears simply disappeared
+          and I felt a level of surety that I'd never felt before.</p>
+          <p>I performed my first trick quite well. They asked to see another, and I performed that, too. After that second trick, they had seen enough and 
+          sent me back upstairs. I waited there with others who auditioned until everyone had a chance. Then came the moment when they called down everyone
+          who had passed. I didn't hear my name at first, and I was disappointed, but then I realized they'd read my name backward, "Burr Matthew," instead
+          of Matthew Burr.</p>
+          <p>I joined the group who returned downstairs and was elated to learn I'd passed and been accepted as a member.</p>
+          <h3>Magic Today</h3>
+          <p>I'm still a member of the Academy, but I live in Texas now, so I don't get to visit the Magic Castle regularly. I haven't performed since that
+          audition, but I still study and learn.</p>
+          <p>I love magic for its secrecy, its cleverness, its history, its beauty, and its effect on us when we watch. I love being part of a fellowship
+          that reaches back to the Egyptians and spans the world from China, to India, to Europe, and to the United States. In magic can be found many
+          lessons about truth, psychology, persuasion, and emotion. I'm grateful I found that magic kit as a young boy.</p>
         </div>
       </div>
     </div>
